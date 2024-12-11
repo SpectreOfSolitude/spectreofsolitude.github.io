663886baa841522ac98398bb588d337905e13cdf
SpectreOfSolitude <kakak16903@gmail.com>
2024-12-11 23:10:38 +0700

    Deploy Next.js to Github Pages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
+module.exports = {
+  output: 'export',
+  basePath: '/nama-repo-anda', // Sesuaikan dengan nama repo GitHub Anda
+  trailingSlash: true,
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
